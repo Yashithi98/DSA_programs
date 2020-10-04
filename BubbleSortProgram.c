fdc736acac35044a8610ba57c40cdc1694b3ba59
Ashish Maharjan <amaharjan1032@gmail.com>
2020-10-04 22:58:13 +0545

    Updated BubbleSortProgram.c

--- BubbleSortProgram.c
+++ BubbleSortProgram.c
@@ -3,30 +3,36 @@
 */
 
 #include<stdio.h>
-int main()
+void BubbleSort(int a[20], int n)
 {
-int n, i, j, temp;
-scanf("%d", &n);
-int arr[n];
-for(i = 0; i < n; i++)
+	int i,j,temp;
+for(i = 0; i <n; i++)
 {
-scanf("%d", &arr[i]);
-}
-for(i = 0; i < n - 1; i++) // to keep track of number of cycles
-{
-for(j = 0; j < n - i - 1; j++) // to compare the elements within the particular cycle
-{
-// swap if one element is greater than its adjacent element
-if(arr[j] > arr[j + 1]) 
-{
-temp = arr[j];
-arr[j] = arr[j + 1];
-arr[j + 1] = temp;
+	for(j = 0; j < n-1; j++)
+	{
+		if(a[j] > a[j+1])
+		{
+		temp = a[j];
+		a[j] = a[j+1];
+		a[j+1] = temp;
+		}
+	}
 }
 }
-}
-for(i = 0; i < n; i++)
+void main()
 {
-printf("%d ", arr[i]);
-}
+	int n,a[20],i,j;
+	printf("Enter the number of terms you want to enter:");
+	scanf("%d",&n);
+	printf("Enter the datas:\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	BubbleSort(a, n);
+	printf("The datas after sorting is:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("\t%d",a[i]);
+	}
 }
